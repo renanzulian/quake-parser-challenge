eea9d8117529332e201dc33f336caaa81909a54c
Renan Zulian <renanzulian@gmail.com>
2020-07-22 11:58:43 -0300

    Lendo o arquivo com o file system

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello Word')
+import * as fs from 'fs';
+import * as path from 'path';
+
+const data = fs.readFileSync(path.join(__dirname + '/../games.log'), 'utf-8')
+
+console.log(data);
