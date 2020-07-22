fd08abbbc9cc4f3c5caaaf46be3eef3c21652894
Renan Zulian <renanzulian@gmail.com>
2020-07-22 19:28:06 -0300

    Trocando o nome de um usuário

--- src/app/components/Game.spec.ts
+++ src/app/components/Game.spec.ts
@@ -1,5 +1,6 @@
 import * as faker from 'faker'
 import Game from './Games'
+import Player from './Player'
 
 describe('Game Entity', () => {
   let game: Game
@@ -9,7 +10,29 @@ describe('Game Entity', () => {
     game = new Game(randomId)
   })
 
-  it('should be instantiated', () => {
+  it('should be defined', () => {
     expect(game).toBeDefined()
+  })
+
+  it('should add many players', () => {
+    const randomRangeId = Array.from(Array(faker.random.number(20)).keys())
+    randomRangeId.forEach((id) => {
+      game.addPlayer(id)
+    })
+    expect(game.players.length).toBe(randomRangeId.length)
+    game.players.forEach((player) => {
+      expect(player).toBeInstanceOf(Player)
+    })
+  })
+
+  it('should update the name of any player', () => {
+    const randomId = faker.random.number(100)
+    const randomName = faker.name.firstName()
+    game.addPlayer(randomId)
+    const player = game.getPlayerById(randomId)
+    expect(player.id).toBe(randomId)
+    expect(player.name).toBe('Unknown')
+    game.updatePlayerName(randomId, randomName)
+    expect(player.name).toBe(randomName)
   })
 })
