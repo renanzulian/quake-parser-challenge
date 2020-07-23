b72086b902a061633b10c814f210ae6d8420b82d
Renan Zulian <renanzulian@gmail.com>
2020-07-23 09:51:06 -0300

    Configurando api basica

---
+++ src/bin/server.ts
@@ -0,0 +1,6 @@
+import http from 'http'
+import app from '../index'
+
+const server = http.createServer(app)
+
+server.listen(3333)
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,41 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import QuakeParser from './app/QuakeParser'
+import express from 'express'
+import Cors from 'cors'
+import Morgan from 'morgan'
 
-const data = fs.readFileSync(path.join(__dirname + '/../games.log'), 'utf-8')
+class App {
+  server: express.Application
+  constructor() {
+    this.server = express()
+    this.middleware()
+    this.routes()
+  }
 
-const app = new QuakeParser(data)
+  middleware() {
+    this.server.use(Cors())
+    this.server.use(express.json())
+    this.server.use(express.urlencoded({ extended: false }))
+    this.server.use(Morgan('dev'))
+  }
 
-app.run()
-console.log(app.ranking)
+  routes() {
+    this.server.get('/', (req, res) => {
+      res.send({ message: 'working' })
+    })
+
+    this.server.use('*', (req, res) => {
+      res.status(400).send({ message: 'Method not found' })
+    })
+  }
+}
+
+export default new App().server
+// import * as fs from 'fs'
+// import * as path from 'path'
+// import QuakeParser from './app/QuakeParser'
+
+// const data = fs.readFileSync(path.join(__dirname + '/../games.log'), 'utf-8')
+
+// const app = new QuakeParser(data)
+
+// app.run()
+// console.log(app.ranking)
